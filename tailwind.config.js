9bef8b19609441738593a8e5f3e660c6b9797ecd
sakib-xrz <sakibxrz21@gmail.com>
2022-09-28 21:57:33 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#5D5FEF",
+
+          secondary: "#D926A9",
+
+          accent: "#1FB2A6",
+
+          neutral: "#191D24",
+
+          "base-100": "#F2F4FA",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
